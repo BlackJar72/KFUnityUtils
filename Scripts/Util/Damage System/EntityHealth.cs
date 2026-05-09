38cff2cd20cc8ad26b64397439211655105668a4
Jared Blackburn <BlackJar72@yahoo.com>
2026-05-09 11:30:35 -0400

    EntityHealth now uses constants instead of literals for regen rates

--- Scripts/Util/Damage System/EntityHealth.cs
+++ Scripts/Util/Damage System/EntityHealth.cs
@@ -6,6 +6,11 @@ namespace kfutils.health {
 
     [Serializable]
     public class EntityHealth {
+
+        public const double HEALING_PAUSE_TIME = 5.0; // in seconds
+        public const double BASE_REGEN_RATE = 1.0;
+        public const double BASE_REGEN_ADJUST = 0.01;
+        
         public static readonly DefaultDamageAdjuster defaultDamageAdjuster = new DefaultDamageAdjuster();
 
         [SerializeField] float baseHealth;
@@ -127,8 +132,9 @@ namespace kfutils.health {
         /// This may be called by the regrn coroutine, or perhaps recreated in it, or even
         /// have the coroutine moved here...?
         /// </summary>
+
         public bool NaturalRegen() {
-            shock = Mathf.Min((shock + ((baseHealth * 0.01f) + 1.0f) * Time.deltaTime), baseHealth);
+            shock = Mathf.Min((float)(shock + ((baseHealth * BASE_REGEN_ADJUST) + BASE_REGEN_RATE) * Time.deltaTime), baseHealth);
             return shock < baseHealth;
         }
 
